e30b4a851b7a62e79544a7e8e6285f9ed8e11bfb
lishaohui <15926535+lishaohui09@user.noreply.gitee.com>
2026-07-13 23:27:14 +0800

    新的板子调试过

--- main/audio/processors/afe_audio_processor.cc
+++ main/audio/processors/afe_audio_processor.cc
@@ -151,7 +151,7 @@ void AfeAudioProcessor::Initialize(AudioCodec* codec, int frame_duration_ms, srm
 
 #ifdef CONFIG_USE_DEVICE_AEC
     afe_config->aec_init = true;
-    afe_config->vad_init = true;
+    afe_config->vad_init = false;
 #else
     afe_config->aec_init = false;
     afe_config->vad_init = true;
@@ -331,7 +331,7 @@ void AfeAudioProcessor::AudioProcessorTask() {
 void AfeAudioProcessor::EnableDeviceAec(bool enable) {
     if (enable) {
 #if CONFIG_USE_DEVICE_AEC
-        afe_iface_->enable_vad(afe_data_);
+        afe_iface_->disable_vad(afe_data_);
         afe_iface_->enable_aec(afe_data_);
 #else
         ESP_LOGE(TAG, "Device AEC is not supported");
